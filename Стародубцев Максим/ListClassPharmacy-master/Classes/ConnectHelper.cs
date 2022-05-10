97a8e4356a7132042f58b271af2c09969b8f0a53
Renc345 <98842888+Renc345@users.noreply.github.com>
2022-05-10 20:57:48 +0300

    Add files via upload

---
+++ Стародубцев Максим/ListClassPharmacy-master/Classes/ConnectHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ListClass.Classes
+{
+    class ConnectHelper
+    {
+        public static List<STUDENT> student = new List<STUDENT>();
+        public static void ReadListFromFile(string filename)
+        {
+            StreamReader streamReader = new StreamReader(filename, Encoding.UTF8);
+            while (!streamReader.EndOfStream)
+            {
+                string line = streamReader.ReadLine();
+                string[] items = line.Split(';');
+                STUDENT students = new STUDENT()
+                {
+                    Name = items[0].Trim(),
+                    Group = items[1].Trim(),
+                    Math = int.Parse(items[2].Trim()),
+                    History = int.Parse(items[3].Trim()),
+                    Physics = int.Parse(items[4].Trim()),
+                    Obzh = int.Parse(items[5].Trim()),
+                    French = int.Parse(items[6].Trim()),
+                };
+                student.Add(students);
+            }
+        }
+        public static void SaveListToFile(string filename)
+        {
+            StreamWriter streamWriter = new StreamWriter(filename, false, Encoding.UTF8);
+            foreach (STUDENT ph in student)
+            {
+                streamWriter.WriteLine($"{ph.Name};{ph.Group};{ph.Math};{ph.History};{ph.Physics};{ph.Obzh};{ph.French}");
+            }
+            streamWriter.Close();
+        }
+    }
+}
---
+++ Стародубцев Максим/ListClassPharmacy-master/WindowAddPreparate.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ListClass.Classes;
+
+namespace ListClass
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowAddPreparate.xaml
+    /// </summary>
+    public partial class WindowAddPreparate : Window
+    {
+        int mode;
+        public WindowAddPreparate()
+        {
+            InitializeComponent();
+            mode = 0;
+        }
+        public WindowAddPreparate(STUDENT students)
+        {
+            InitializeComponent();
+            TxbName.Text = students.Name;
+            TxbGroup.Text = students.Group;
+            TxbMath.Text = students.Math.ToString();
+            TxbHistory.Text = students.History.ToString();
+            TxbPhysics.Text = students.Physics.ToString();
+            TxbObzh.Text = students.Obzh.ToString();
+            TxbFrench.Text = students.French.ToString();
+            mode = 1;
+            BtnAddPreparate.Content = "Сохранить";
+        }
+        /// <summary>
+        /// Привязка полей для добавления
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private void BtnAddPreparate_Click(object sender, RoutedEventArgs e)
+        {
+            if (mode == 0)
+            {
+                try
+                {
+                    STUDENT student = new STUDENT()
+                    {
+                        Name = TxbName.Text,
+                        Group = TxbGroup.Text,
+                        Math = int.Parse(TxbMath.Text),
+                        History = int.Parse(TxbHistory.Text),
+                        Physics = int.Parse(TxbPhysics.Text),
+                        Obzh = int.Parse(TxbObzh.Text),
+                        French = int.Parse(TxbFrench.Text),
+                    };
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Проверьте входные данные: {ex}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                try
+                {
+                    for (int i = 0; i < ConnectHelper.student.Count; i++)
+                    {
+                        if (ConnectHelper.student[i].Name == TxbName.Text)
+                        {
+                            ConnectHelper.student[i].Group = TxbGroup.Text;
+                            ConnectHelper.student[i].Math = int.Parse(TxbMath.Text);
+                            ConnectHelper.student[i].History = int.Parse(TxbHistory.Text);
+                            ConnectHelper.student[i].Physics = int.Parse(TxbPhysics.Text);
+                            ConnectHelper.student[i].Obzh = int.Parse(TxbObzh.Text);
+                            ConnectHelper.student[i].French = int.Parse(TxbFrench.Text);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Проверьте входные данные: {ex}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            ConnectHelper.SaveListToFile(@"ListPreparates.txt");
+            this.Close();
+        }
+    }
+}
